5425d9c153da3ea9d8e033efeec9c141acdb1685
김지은 <kimmjieun@gmail.com>
2021-10-05 13:31:22 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+    // 이렇게 단위 테스트를 진행한다 단위테스트 = 순수하게 자바 코드로 테스트하는 것
+}
