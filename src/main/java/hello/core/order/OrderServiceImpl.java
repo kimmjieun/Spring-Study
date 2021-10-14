06846491491c8a08cd9cfcc4abc086a866f89603
김지은 <kimmjieun@gmail.com>
2021-10-14 11:22:28 +0900

    @Autowired 필드 명, @Qualifier, @Primary (예제: @Qualifier)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,14 +4,21 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // 롬복 애노테이션 -> final 붙은 객체에 대해 생성자를 만들어줘
+//@RequiredArgsConstructor // 롬복 애노테이션 -> final 붙은 객체에 대해 생성자를 만들어줘
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository; //final을 쓰면 생성자 기본으로 할당되어야함
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존함
 
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice){
         Member member = memberRepository.findById(memberId);
