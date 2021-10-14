be4ba3548132cfeb4b3dbb0070cbeb0048dd1ed0
김지은 <kimmjieun@gmail.com>
2021-10-14 10:24:37 +0900

    롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,31 +3,14 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 롬복 애노테이션 -> final 붙은 객체에 대해 생성자를 만들어줘
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository; //final을 쓰면 생성자 기본으로 할당되어야함
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존함
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-    // 생성자를 통해 위의 선언한 곳에 값이 할당 됨
-
-
-//    // 수정자 주입
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice){
--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -5,11 +5,11 @@ import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceImplTest {
-    @Test
-    void createOrder() {
-
-        OrderServiceImpl orderService =  new OrderServiceImpl(); // impl 잘 만들었는지
-        orderService.createOrder(1L, "itemA", 10000);
-    }
+//    @Test
+//    void createOrder() {
+//
+//        OrderServiceImpl orderService =  new OrderServiceImpl(); // impl 잘 만들었는지
+//        orderService.createOrder(1L, "itemA", 10000);
+//    }
 
 }
